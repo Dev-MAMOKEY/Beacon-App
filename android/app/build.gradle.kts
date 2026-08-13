388549495f974c6e2c47023f2f6280ccea6dfd8e
김도훈 <dohun0310@naver.com>
2026-08-13 18:58:36 +0900

    [Chore/#2] Chore: flutter_secure_storage 요구사항에 맞춰 compileSdk를 37로 상향

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.mamokey.beacon"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 37
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
